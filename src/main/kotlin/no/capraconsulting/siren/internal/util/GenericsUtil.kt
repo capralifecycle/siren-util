4cd9a22335dce7b1e13788368db6c4ba21f74492
Øystein Krogstie <oyk@capraconsulting.no>
2025-05-09 16:10:48 +0200

    Bump versions

--- src/main/kotlin/no/capraconsulting/siren/internal/util/GenericsUtil.kt
+++ src/main/kotlin/no/capraconsulting/siren/internal/util/GenericsUtil.kt
@@ -11,11 +11,13 @@ internal fun Any?.asNonNullStringList(): List<String> {
     require(item is String) { "Casting to List of Strings. Found item ${item?.javaClass}" }
   }
 
-  @Suppress("UNCHECKED_CAST") return this as List<String>
+  @Suppress("UNCHECKED_CAST")
+  return this as List<String>
 }
 
 internal fun Any?.asMap(): Map<String, Any?> {
   require(this is Map<*, *>) { "Casting to Map failed. Found type ${this?.javaClass}" }
 
-  @Suppress("UNCHECKED_CAST") return this as Map<String, Any>
+  @Suppress("UNCHECKED_CAST")
+  return this as Map<String, Any>
 }
